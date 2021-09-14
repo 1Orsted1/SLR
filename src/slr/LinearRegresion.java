9a877be255295a61138e99374a5a9cdaf07540cf
1Orsted1 <45337493+1Orsted1@users.noreply.github.com>
2021-09-14 09:21:47 -0500

    Resolviendo un bug con B0

---
+++ src/slr/LinearRegresion.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slr;
+
+/**
+ *
+ * @author ftm
+ */
+public class LinearRegresion {
+
+    private double dataset[][];
+    private double sumx, sumy, sumxy, sumxsq, x;
+
+    public LinearRegresion(double[][] dataset, double x) {
+        this.dataset = dataset;
+        this.x = x;
+        Generate();
+    }
+
+    private void Generate() {
+        for (int i = 0; i < dataset.length; i++) {
+            sumx = dataset[i][1];
+            sumy = dataset[i][0];
+            sumxsq = dataset[i][1] * dataset[i][1];
+            sumxy = dataset[i][1] * dataset[i][0];
+        }
+    }
+
+    private double B0() {
+        return (sumxsq * sumy - sumx * sumxy) / (dataset.length * sumxsq - (sumx * sumx));
+    }
+
+    private double B1() {
+        return (dataset.length * sumxy - sumx * sumy) / (dataset.length * sumxsq - (sumx * sumx));
+    }
+
+    private double CalculateYH() {
+        return (B0() + B1() * x);
+    }
+
+    //regresion ecuation
+    public void RE() {
+        System.out.println("Y" + (char) 94 + "=" + B0() + "+"+B1() + "(" + x + ")");
+    } 
+    
+    public void PrintYH() {
+        System.out.println("Y" + (char) 94 + "="+ CalculateYH());
+    } 
+    
+}
---
+++ src/slr/SLR.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slr;
+
+import java.io.File;
+import java.util.Scanner;
+import slr.dummyData;
+/**
+ *
+ * @author ftm
+ */
+public class SLR {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     dummyData Data = new dummyData();
+     final double [][] dataset = Data.getDummyData();
+     LinearRegresion SLP;
+     double x;
+     
+     System.out.println("Please introduce the x value");
+     x = sc.nextDouble();
+     System.out.println (new File (".").getAbsolutePath ());
+     SLP = new LinearRegresion(dataset, x);
+     
+     SLP.RE();
+     SLP.PrintYH();
+     
+
+      
+        
+    }
+    
+}
